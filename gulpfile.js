125c0f46fb4b5eb49f30fff4fc8e25da18a17479
Ruben Rica <r.ricamarti@gmail.com>
2017-02-24 13:51:21 +0100

    Keep angular dependencies in the vendor JS folder with a gulp task

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+
+gulp.task('default', () => {
+  gulp
+    .src('./node_modules/angular/angular.min.js')
+    .pipe(gulp.dest('./js/vendor/'))
+
+  gulp
+    .src('./node_modules/angular-route/angular-route.min.js')
+    .pipe(gulp.dest('./js/vendor/'));
+});

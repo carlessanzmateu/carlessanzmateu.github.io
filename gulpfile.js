6f69f9ce55237c85d35e3667fb86bbf894c2cf0d
carlessanzmateu <carlessanzmateu@gmail.com>
2017-02-24 21:14:49 +0100

    Sass and new angular dependencies add to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
+'use strict';
+
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 gulp.task('default', () => {
   gulp
@@ -8,4 +11,27 @@ gulp.task('default', () => {
   gulp
     .src('./node_modules/angular-route/angular-route.js')
     .pipe(gulp.dest('./js/vendor/'));
+
+  gulp
+    .src('./node_modules/angular-aria/angular-aria.js')
+    .pipe(gulp.dest('./js/vendor/'));
+
+  gulp
+    .src('./node_modules/angular-animate/angular-animate.js')
+    .pipe(gulp.dest('./js/vendor/'));
+
+  gulp
+    .src('./node_modules/angular-material/angular-material.js')
+    .pipe(gulp.dest('./js/vendor/'));
+});
+
+gulp.task('sass', () => {
+  gulp
+    .src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', () => {
+  gulp.watch('./scss/**/*.scss', ['sass']);
 });

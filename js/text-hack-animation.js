e16eec4462c3adc874dc591b63c8e7176ebcb903
carlessanzmateu <carlessanzmateu@gmail.com>
2017-02-26 17:30:20 +0100

    text animation project library

---
+++ js/text-hack-animation.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function () {
+  window.onload = function () {
+    //var hackableText = document.getElementById('title-hack-animated').textContent;
+    var foo = document.getElementById('foo');
+    //console.log(hackableText);
+    console.log(foo);
+  };
+})();

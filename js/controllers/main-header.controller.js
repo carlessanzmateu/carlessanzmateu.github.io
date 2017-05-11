d81f3f9390685c97a6f64c7e68054de28032f502
carlessanzmateu <carlessanzmateu@gmail.com>
2017-05-11 17:55:02 +0200

    Controller for main Header
    *This controller it's now created for add hackAnimation

---
+++ js/controllers/main-header.controller.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  angular.module('carlessanzmateuPortfolio')
+    .controller('mainHeaderController', mainHeaderController);
+
+    function mainHeaderController() {
+      hackAnimation.hackIdAnimation("foo");
+    }
+})();

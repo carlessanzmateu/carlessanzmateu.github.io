38ac1fdef0b0a0dae24bd9a75b40d7a3cacaa581
carlessanzmateu <carlessanzmateu@gmail.com>
2017-05-11 17:54:16 +0200

    Hack Animation Library update
    * Hack Animation object added to a global object
    * Now the hack library its accesible from all the app

--- js/text-hack-animation.js
+++ js/text-hack-animation.js
@@ -1,10 +1,45 @@
-'use strict';
+(function() {
 
-(function () {
-  window.onload = function () {
-    //var hackableText = document.getElementById('title-hack-animated').textContent;
-    var foo = document.getElementById('foo');
-    //console.log(hackableText);
-    console.log(foo);
+  window.onload = function() {
+    //var textsToAnimate = document.getElementsByClassName('hackAnimation');
+
+    /*for(var i = 0; i < textsToAnimate.length; i++) {
+      individualText = textsToAnimate[i].textContent;
+      lettersToChange = getRandomArbitrary(1,individualText.length);
+    }*/
+
+
+
+  }
+})();
+
+(function() {
+  'use strict';
+
+  var HACKEDCHARSMAP = ['A','$','#','/','¿','¬','«','µ','Æ'];
+  var hackAnimation = {
+    hackIdAnimation: hackId,
+    hackClassAnimation: hackClass,
   };
+  window.hackAnimation = hackAnimation;
+
+  function hackId(id) {
+    var idElementToHack = document.getElementById(id);
+    var unhackedText = idElementToHack.textContent;
+    var textLength = unhackedText.length;
+    var letterToHack = getRandomIntegerNumber(0, textLength);
+    var hackedText = unhackedText.replace(unhackedText[letterToHack],
+      HACKEDCHARSMAP[getRandomIntegerNumber(0, HACKEDCHARSMAP.length-1)]);
+
+    idElementToHack.textContent = hackedText;
+  }
+
+  function hackClass(selectedDOMClass) {
+    var classElementsToHack = document.getElementsByClassName(selectedDOMClass);
+    console.log(classElementsToHack);
+  }
+
+  function getRandomIntegerNumber(min, max) {
+    return parseInt(Math.random() * (max - min) + min);
+  }
 })();

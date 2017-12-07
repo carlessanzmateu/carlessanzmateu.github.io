d07d58e4af9a50dbe085e5d599e0851dbdfd184a
Carles Sanz Mateu <csanz.soprasteria@gmail.com>
2017-12-07 15:03:07 +0100

    concat css files in one file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
 
 gulp.task('default', () => {
   gulp
@@ -11,6 +13,17 @@ gulp.task('default', () => {
   gulp
     .src('./node_modules/angular-route/angular-route.js')
     .pipe(gulp.dest('./js/vendor/'));
+
+  gulp.src('./css/**/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('./dist'));
+
+    // gulp.src('./css/**/*.css')
+    // .pipe(minifyCSS())
+    // .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+    // .pipe(concat('style.min.css'))
+    // .pipe(gulp.dest('./dist/css'))
 });
 
 gulp.task('sass', () => {
@@ -22,4 +35,8 @@ gulp.task('sass', () => {
 
 gulp.task('sass:watch', () => {
   gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+gulp.task('minify-css', () => {
+  return 
 });

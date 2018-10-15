033f10ee9247a41825ac6a77753d3c7eed42e96c
carles <carles.sanz.contractor@dev.bbva.com>
2018-10-15 15:33:50 +0200

    refactor views folder structure

---
+++ src/views/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Cover msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Cover from '@/views/home/components/Cover.component.vue'
+
+export default {
+  name: "home",
+  components: {
+    Cover
+  }
+};
+</script>
---
+++ src/views/home/components/Cover.component.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="cover">
+    <h1>Hi! I'm working as a charm hello world!</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Cover",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.cover {
+  height: 100vh;
+  width: 100vw;
+  background-color: red;
+}
+</style>

f6bb7ef262868c65d975751fe1ba7f1949ea98d0
carlessanzmateu <carlessanzmateu@gmail.com>
2018-12-05 20:49:06 +0100

    rebuild dist and remove to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 git commit -am "Save uncommited changes (WIP)"
 git branch --delete --force gh-pages
 git checkout --orphan gh-pages
+rm -rf ./dist/node_modules
 git add -f dist
 git commit -m "Rebuild GitHub pages"
 git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout -

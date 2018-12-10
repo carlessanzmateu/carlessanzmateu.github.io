63e956039de89efee8ed8b1389d9a776d9259f24
carles <carles.sanz.contractor@dev.bbva.com>
2018-12-10 16:45:18 +0100

    improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+echo 'add all SEO files'
 cp CNAME dist/
 cp robots.txt dist/
 cp sitemap.xml dist/
+
+echo 'add git tracking to dist'
 cp -r .git dist/
+
+echo 'moving to dist'
 cd dist/
+
+echo 'adding and commiting'
 git add .
 git commit -m "deploying dist in master"
+
+echo 'pushing branch'
 git push origin master
+
+echo 'script finished!'

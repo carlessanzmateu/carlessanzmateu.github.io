c8dcbc0451b508bd8fa9743c75574f1691c50561
carles <carles.sanz.contractor@dev.bbva.com>
2018-12-11 12:35:15 +0100

    now navigation fix scroll on page top

--- src/router.js
+++ src/router.js
@@ -24,5 +24,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return {
+      x: 0,
+      y: 0
+    }
+  }
 })
